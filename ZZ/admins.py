e20d2680f1e277c560ebbaec233ed5c04a81229c
It's Hussain <90657846+The-Death-Soul@users.noreply.github.com>
2022-06-11 15:56:08 +0530

    Update admins.py

--- ZZ/admins.py
+++ ZZ/admins.py
@@ -1,13 +1,20 @@
-from typing import Dict, List
+from typing import List
+from pyrogram.types import Chat
+from ZZ.Cache.admins import get as gett, set
 
-admins: Dict[int, List[int]] = {}
+async def get_administrators(chat: Chat) -> List[int]:
+    get = gett(chat.id)
 
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
 
-def set(chat_id: int, admins_: List[int]):
-    admins[chat_id] = admins_
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
 
+        set(chat.id, to_set)
+        return await get_administrators(chat)
 
-def get(chat_id: int) -> List[int]:
-    if chat_id in admins:
-        return admins[chat_id]
-    return []
