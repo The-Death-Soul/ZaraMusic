3f2c51eae142242f2b1794ac21b6b5704e919b3f
It's Hussain <90657846+The-Death-Soul@users.noreply.github.com>
2022-06-11 13:01:42 +0530

    Create smex.py

---
+++ ZaraLina/smex.py
@@ -0,0 +1,53 @@
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from ZZ.filters import command
+
+@Client.on_message(command(["wish", f"wish@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["zain", f"zain@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`❤️𝗭𝗮𝗶𝗻 ❣️ 𝗭𝗮𝗿𝗮❤️...`")
+
+
+@Client.on_message(command(["zara", f"zara@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`❤️𝗭𝗮𝗿𝗮 ❣️ 𝗭𝗮𝗶𝗻❤️...`")
+
+
+@Client.on_message(command(["truth", f"truth@{BOT_USERNAME}"]))
+async def truth(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/truth").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
+
+
+@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+async def dare(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/dare").json()
+        results = f"{resp['message']}"
+        return await message.reply_text(results)
+    except Exception:
+        await message.reply_text("something went wrong...")
